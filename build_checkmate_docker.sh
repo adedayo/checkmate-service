bfc217b034064ec689232768e4ca4f2cb3ba71b6
Adedayo Adetoye <3177640+adedayo@users.noreply.github.com>
2022-04-19 21:53:22 +0100

    Use a different cloning strategy

--- build_checkmate_docker.sh
+++ build_checkmate_docker.sh
@@ -1,36 +1,38 @@
 #!/bin/sh
 
-# mkdir -p tmp
-# pushd tmp
-
 # download checkmate app and services
-repos=( "checkmate" ) 
-tag_repos=( "git-service-driver:v0.1.2" "checkmate-app:v0.2.3")
+# repos=( "checkmate" ) 
+# tag_repos=( "git-service-driver:v0.1.2" "checkmate-app:v0.2.3")
 
-for repo in "${repos[@]}"
-do
-    tarball=$(curl -s "https://api.github.com/repos/adedayo/${repo}/releases/latest" | grep tarball_url | cut -d '"' -f 4)
+# for repo in "${repos[@]}"
+# do
+#     tarball=$(curl -s "https://api.github.com/repos/adedayo/${repo}/releases/latest" | grep tarball_url | cut -d '"' -f 4)
 
-    echo $tarball
+#     echo $tarball
 
-    curl -LJ "$tarball" -o "${repo}".tar.gz
-    mkdir -p "${repo}"
-    tar xvfz "${repo}".tar.gz -C "${repo}" --strip-components 1
-    rm "${repo}".tar.gz 
-done
+#     curl -LJ "$tarball" -o "${repo}".tar.gz
+#     mkdir -p "${repo}"
+#     tar xvfz "${repo}".tar.gz -C "${repo}" --strip-components 1
+#     rm "${repo}".tar.gz 
+# done
 
-for repo in "${tag_repos[@]}"
-do
-    version=$(echo $repo | cut -d : -f2 )
-    repo=$(echo $repo | cut -d : -f1 )
-    tarball="https://api.github.com/repos/adedayo/${repo}/tarball/refs/tags/${version}"
-    echo $tarball
+# for repo in "${tag_repos[@]}"
+# do
+#     version=$(echo $repo | cut -d : -f2 )
+#     repo=$(echo $repo | cut -d : -f1 )
+#     tarball="https://api.github.com/repos/adedayo/${repo}/tarball/refs/tags/${version}"
+#     echo $tarball
 
-    curl -LJ "$tarball" -o "${repo}".tar.gz
-    mkdir -p "${repo}"
-    tar xvfz "${repo}".tar.gz -C "${repo}" --strip-components 1
-    rm "${repo}".tar.gz
-done
+#     curl -LJ "$tarball" -o "${repo}".tar.gz
+#     mkdir -p "${repo}"
+#     tar xvfz "${repo}".tar.gz -C "${repo}" --strip-components 1
+#     rm "${repo}".tar.gz
+# done
+
+#clone repositories
+docker run -ti --rm  -v $(pwd):/git alpine/git clone https://github.com/adedayo/checkmate.git
+docker run -ti --rm  -v $(pwd):/git alpine/git clone https://github.com/adedayo/git-service-driver.git
+docker run -ti --rm  -v $(pwd):/git alpine/git clone https://github.com/adedayo/checkmate-app.git
 
 checkmate_host=$(hostname)
 
@@ -53,6 +55,7 @@ CHECKMATE_DATA=/var/lib/checkmate
 CHECKMATE_PORT=80
 CHECKMATE_APP_SERVERNAME=${checkmate_host}
 CHECKMATE_API_PORT=17283
+BUILD_LOCATION=$(pwd)
 EOF
 
 # Docker compose
